7de302c1c6a0e22638100e5d34598d9ab2577ab2
Index Exchange <adapter-certification-eng@indexexchange.com>
2018-05-10 16:23:03 -0400

    GDPR template update

--- rtk-htb.js
+++ rtk-htb.js
@@ -25,6 +25,7 @@ var SpaceCamp = require('space-camp.js');
 var System = require('system.js');
 var Network = require('network.js');
 var Utilities = require('utilities.js');
+var ComplianceService;
 var EventsService;
 var RenderService;
 
@@ -147,8 +148,36 @@ function RtkHtb(configs) {
         /* Change this to your bidder endpoint.*/
         var baseUrl = Browser.getProtocol() + '//someAdapterEndpoint.com/bid';
 
+        /* ------------------------ Get consent information -------------------------
+         * If you want to implement GDPR consent in your adapter, use the function
+         * ComplianceService.gdpr.getConsent() which will return an object.
+         *
+         * Here is what the values in that object mean:
+         *      - applies: the boolean value indicating if the request is subject to
+         *      GDPR regulations
+         *      - consentString: the consent string developed by GDPR Consent Working
+         *      Group under the auspices of IAB Europe
+         *
+         * The return object should look something like this:
+         * {
+         *      applies: true,
+         *      consentString: "BOQ7WlgOQ7WlgABABwAAABJOACgACAAQABA"
+         * }
+         *
+         * You can also determine whether or not the publisher has enabled privacy
+         * features in their wrapper by querying ComplianceService.isPrivacyEnabled().
+         * 
+         * This function will return a boolean, which indicates whether the wrapper's
+         * privacy features are on (true) or off (false). If they are off, the values
+         * returned from gdpr.getConsent() are safe defaults and no attempt has been
+         * made by the wrapper to contact a Consent Management Platform.
+         */
+        var gdprStatus = ComplianceService.gdpr.getConsent();
+        var privacyEnabled = ComplianceService.isPrivacyEnabled();
+
         /* ---------------- Craft bid request using the above returnParcels --------- */
 
+        /* ------- Put GDPR consent code here if you are implementing GDPR ---------- */
 
         /* -------------------------------------------------------------------------- */
 
@@ -377,6 +406,7 @@ function RtkHtb(configs) {
      * ---------------------------------- */
 
     (function __constructor() {
+        ComplianceService = SpaceCamp.services.ComplianceService;
         EventsService = SpaceCamp.services.EventsService;
         RenderService = SpaceCamp.services.RenderService;
 
@@ -392,7 +422,7 @@ function RtkHtb(configs) {
             partnerId: 'RtkHtb', // PartnerName
             namespace: 'RtkHtb', // Should be same as partnerName
             statsId: 'RTK', // Unique partner identifier
-            version: '2.1.1',
+            version: '2.0.0',
             targetingType: 'slot',
             enabledAnalytics: {
                 requestTime: true
